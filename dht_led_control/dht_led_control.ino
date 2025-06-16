18c9c22e34222c3ba7c50b4526131247548dce41
P-Odiix <mahingarodin@gmail.com>
2025-06-16 15:53:03 +0200

    [set up the arduino codes]

---
+++ dht_led_control/dht_led_control.ino
@@ -0,0 +1,82 @@
+#include<DHT.h>
+
+#define DHTPIN 3
+#define DHTPOWER 4
+#define DHTGROUND 2
+#define LEDPIN 13
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(DHTPOWER, OUTPUT);
+  pinMode(DHTGROUND, OUTPUT);
+  digitalWrite(DHTPOWER, HIGH);
+  digitalWrite(DHTGROUND, LOW);
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, LOW);
+  dht.begin();
+}
+
+void loop() {
+  //reading sernsor data every 2 secs
+  static unsigned long lastRead = 0;
+  if(millis() - lastRead >= 2000) {
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
+    if(!isnan(temp) && !isnan(hum)) {
+      Serial.print("{\"temperature\":");
+      Serial.print(temp);
+      Serial.print(",\"humidity\":");
+      Serial.print(hum);
+      Serial.println("}");
+    }
+    lastRead = millis();
+  }
+
+  //checking incoming uart commands
+  if(Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    if(command == "{\"led\":\"ON\"}") {
+      digitalWrite(LEDPIN, HIGH);
+    } else if (command = "{\"led\":\"OFF\"}") {
+      digitalWrite(LEDPIN, LOW);
+    }
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
