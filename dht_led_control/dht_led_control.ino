c9ac83024485f62aeab52a96f3788fb3f509a6d4
P-Odiix <mahingarodin@gmail.com>
2025-06-16 16:19:17 +0200

    [renewed]

--- dht_led_control/dht_led_control.ino
+++ dht_led_control/dht_led_control.ino
@@ -1,14 +1,12 @@
-#include<DHT.h>
+#include <DHT.h>
 
-#define DHTPIN 3
-#define DHTPOWER 4
-#define DHTGROUND 2
-#define LEDPIN 13
-#define DHTTYPE DHT11
+#define DHTPIN 3       
+#define DHTPOWER 4   
+#define DHTGROUND 2    
+#define LEDPIN 13      
+#define DHTTYPE DHT11   
 
 DHT dht(DHTPIN, DHTTYPE);
-
-
 
 void setup() {
   Serial.begin(9600);
@@ -22,12 +20,11 @@ void setup() {
 }
 
 void loop() {
-  //reading sernsor data every 2 secs
   static unsigned long lastRead = 0;
-  if(millis() - lastRead >= 2000) {
+  if (millis() - lastRead >= 2000) {
     float temp = dht.readTemperature();
     float hum = dht.readHumidity();
-    if(!isnan(temp) && !isnan(hum)) {
+    if (!isnan(temp) && !isnan(hum)) {
       Serial.print("{\"temperature\":");
       Serial.print(temp);
       Serial.print(",\"humidity\":");
@@ -37,46 +34,13 @@ void loop() {
     lastRead = millis();
   }
 
-  //checking incoming uart commands
-  if(Serial.available() > 0) {
+  if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
     command.trim();
-    if(command == "{\"led\":\"ON\"}") {
+    if (command == "{\"led\":\"ON\"}") {
       digitalWrite(LEDPIN, HIGH);
-    } else if (command = "{\"led\":\"OFF\"}") {
+    } else if (command == "{\"led\":\"OFF\"}") {
       digitalWrite(LEDPIN, LOW);
     }
   }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
